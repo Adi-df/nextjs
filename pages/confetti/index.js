2d194458f99545635b91c8068cdb15c7b36534b9
Adi-df <adidf-web@laposte.net>
2021-01-15 19:10:57 +0100

    Create classic cannon

--- pages/confetti/index.js
+++ pages/confetti/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Confetti from "canvas-confetti";
+import Cannon from "../../components/cannon";
 import Homebutton from "../../components/homebutton";
 
 export default function Home() {
-    let cannons = {};
+    let cannons = {
+        classic: () => Confetti(),
+    };
 
     return (
         <div className="text-center p-5">
@@ -13,7 +16,7 @@ export default function Home() {
             </Head>
             <Homebutton handleClick={() => Confetti.reset()} />
             <h1 className="text-red-600 text-5xl">Hello Confetti</h1>
-            <div className="my-10 flex w-full h-full justify-between flex-col">
+            <div className="mt-10 flex w-full h-full justify-between flex-col">
                 <h2 className="text-2xl mb-5">Play with cannon confetti</h2>
                 <button
                     className="rounded-full p-2 my-8 text-xl text-white border-yellow-500 bg-yellow-500 hover:bg-yellow-600"
@@ -22,6 +25,7 @@ export default function Home() {
                     Clear
                 </button>
             </div>
+            <Cannon sliders={[]} name={"Classic"} handler={cannons.classic} />
         </div>
     );
 }
