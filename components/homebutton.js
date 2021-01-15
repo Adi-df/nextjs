c71d733c8380b6f6ff4f8790156dd8c7243dca84
Adi-df <adidf-web@laposte.net>
2021-01-15 19:40:27 +0100

    Better Responsive

--- components/homebutton.js
+++ components/homebutton.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Homebutton({ handleClick }) {
     return (
-        <div className="w-10 h-10 bg-green-400 flex justify-center items-center rounded-full text-white text-xl absolute">
+        <div className="w-10 h-10 bg-green-400 flex justify-center items-center rounded-full text-white text-xl sm:absolute relative">
             <Link href={"/"}>
                 <a onClick={handleClick}>{"<"}</a>
             </Link>
