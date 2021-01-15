f2b1c94624bdb3b21c4ea134b58a363ac4119d5c
Adi-df <adidf-web@laposte.net>
2021-01-15 19:40:27 +0100

    Better Responsive

--- components/cannon.js
+++ components/cannon.js
@@ -10,22 +10,24 @@ export default function Cannon({ sliders, name, handler }) {
     }));
 
     return (
-        <div className="pt-6 w-full flex justify-center items-center">
-            {slidersStates.map((slider) => (
-                <div className="flex justify-center mr-5" key={slider.name}>
-                    <input
-                        className="border-red-500 w-20 p-2 text-white bg-red-500 rounded-full"
-                        type="number"
-                        value={slider.state[0]}
-                        min={slider.min}
-                        max={slider.max}
-                        step={slider.step}
-                        onChange={(e) =>
-                            slider.state[1](parseInt(e.target.value))
-                        }
-                    />
-                </div>
-            ))}
+        <div className="pt-6 w-full flex justify-center items-center flex-col sm:flex-row">
+            <div className="flex sm:mb-0 mb-2">
+                {slidersStates.map((slider) => (
+                    <div className="flex justify-center mr-5" key={slider.name}>
+                        <input
+                            className="border-red-500 w-20 p-2 text-white bg-red-500 rounded-full"
+                            type="number"
+                            value={slider.state[0]}
+                            min={slider.min}
+                            max={slider.max}
+                            step={slider.step}
+                            onChange={(e) =>
+                                slider.state[1](parseInt(e.target.value))
+                            }
+                        />
+                    </div>
+                ))}
+            </div>
 
             <button
                 className="rounded-full p-2 w-full text-xl text-white border-blue-500 bg-blue-500 hover:bg-blue-700"
--- components/homebutton.js
+++ components/homebutton.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Homebutton({ handleClick }) {
     return (
-        <div className="w-10 h-10 bg-green-400 flex justify-center items-center rounded-full text-white text-xl absolute">
+        <div className="sm:w-10 h-10 sm:mb-0 bg-green-400 flex justify-center items-center sm:rounded-full text-white text-xl sm:absolute static rounded-none w-full mb-2">
             <Link href={"/"}>
                 <a onClick={handleClick}>{"<"}</a>
             </Link>
