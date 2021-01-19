44a0216884f15c460a45a2101aee203a8925ae7a
Adi-df <adidf-web@laposte.net>
2021-01-19 19:32:56 +0100

    Add reponsponsive margins to links

--- components/homelink.js
+++ components/homelink.js
@@ -1,8 +1,12 @@
 import Link from "next/link";
 
-export default function HomeLink({ name, href }) {
+export default function HomeLink({ name, href, className }) {
     return (
-        <span className="text-2xl p-10 rounded bg-green-400 text-white">
+        <span
+            className={"text-2xl p-10 rounded bg-green-400 text-white mr-4 mt-4 ".concat(
+                className || ""
+            )}
+        >
             <Link href={href}>{name}</Link>
         </span>
     );
