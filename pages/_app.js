b0d21287ffbf3168e121df9fefe584a89481e479
Adi-df <adidf-web@laposte.net>
2021-01-24 19:28:18 +0100

    Add Easter Eggs Collection

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
+import Head from "next/head";
+
 import "../styles/master.css";
 import "tailwindcss/tailwind.css";
 
 function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <Head>
+                <script
+                    src="https://rawgit.com/WeiChiaChang/Easter-egg/master/easter-eggs-collection.js"
+                    defer
+                />
+            </Head>
+            <Component {...pageProps} />
+        </div>
+    );
 }
 
 export default App;
